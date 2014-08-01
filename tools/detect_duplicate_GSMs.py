5da01da31ad31b48fdab6387030e48bc832c38b0
Zhuyi Xue (bcgsc) <alfred532008@gmail.com>
2014-08-01 14:38:58 -0700

    refactored detect_duplicate_GSMs.py

--- tools/detect_duplicate_GSMs.py
+++ tools/detect_duplicate_GSMs.py
@@ -1,63 +1,93 @@
 #!/usr/bin/env python
 
-import sys
-import csv
-
 """
-This script detects duplicate GSMs within one GSE from the xlsx/csv files
-provided by the collaborator
+This script detects duplicated GSMs within one GSE in GSE_GSM.csv
 
 Please remove duplicates mannually (if practical) and save the file to a new
-csv file with name like RNAseq_batchn.processed.csv
+GSE_GSM.csv file
 """
 
-from objs import GEORecord
+import csv
+import re
+import argparse
 
-def print_header():
-    print "{0:15s} {1:20s} {2:20s} {3:20s} {4:20s} {5}".format(
-        'Invalid GSE', '# used_samples', '# total_samples',
-        '# GSMs', 'set(# GSMs)', 'duplicated GSMs')
+def read(input_csv):
+    """
+    Read csv and yield gse and gsm in pair
 
-def print_summary(batch_csv, has_invalid_GSE, count, set_count):
-    print "=" * 79
-    if has_invalid_GSE:
-        print '!!! Remove invalid GSE(s) in {0} before proceeding'.format(
-            batch_csv)
-    else:
-        print '{0} is clean'.format(batch_csv)
-    print "total # GSMs: {0}".format(count)
-    print "total set(# GSMs): {0}".format(set_count)
-    print "=" * 79
-
-def main(batch_csv):
-    has_invalid_GSE = False
-    count, set_count = 0, 0
-    with open(batch_csv, 'rb') as inf:
-        inf.readline()          # skip the headers
+    :param input_csv: the csv with only information of GSE and GSM with one GSE
+    per row, and GSMs are separated by semicolon
+    check example_GSE_GSM.txt for example.
+    """
+    with open(input_csv, 'rb') as inf:
         csv_reader = csv.reader(inf)
-        for row in csv_reader:
-            geo_record = GEORecord(*row)
-            GSMs = geo_record.GSMs
-            num, set_num = len(GSMs), len(set(GSMs))
-            if num != set_num or geo_record.num_used_samples != set_num:
-                if not has_invalid_GSE:
-                    print_header()
-                    has_invalid_GSE = True
-                duplicated_GSMs = [_ for _ in GSMs if GSMs.count(_) > 1]
-                print "{0:15s} {1:<20d} {2:<20d} {3:<20d} {4:<20d} {5}".format(
-                    geo_record.GSE,
-                    geo_record.num_used_samples,
-                    geo_record.num_used_samples,
-                    num, set_num, ' '.join(duplicated_GSMs))
-            count += num
-            set_count += set_num
+        for k, row in enumerate(csv_reader):
+            if row:             # not a blank line
+                res = process(k+1, row)
+                if res is not None: # could be None in cases of invalid rows
+                    gse, gsms = res
+                    for gsm in gsms:
+                        yield gse, gsm
 
-    print_summary(batch_csv, has_invalid_GSE, count, set_count)
+
+def process(k, row):
+    """
+    Process each row and return gse, and gsms, including basic sanity
+    checking
+
+    :param k: row number. i.e. 1, 2, 3, 4, ...
+    :param row: csv row value as a list
+
+    """
+    def err():
+        print ('Ignored invalid row ({0}): {1}. Please check the format '
+               'of example_GSE_GSM.csv'.format(k, row))
+
+    # check if there are only two columns
+    if len(row) != 2:
+        err()
+        return
+
+    gse, gsms = row
+    gsms = [_.strip() for _ in gsms.split(';')]
+    # check if GSE is properly named
+    if not re.search('^GSE\d+', gse):
+        err()
+        return
+    # check if GSMs are properly named
+    if not all(re.search('^GSM\d+', _) for _ in gsms):
+        err()
+        return
+    return gse, gsms
+
+def main():
+    options = parse_args()
+    input_csv = options.input_csv
+
+    flag = True
+    current_gse = None
+    for gse, gsm in read(input_csv):
+        if current_gse is None or gse != current_gse:
+            all_gsm = []
+            current_gse = gse
+        if gsm not in all_gsm:
+            all_gsm.append(gsm)
+        else:
+            print 'duplicated GSM: {0} from {1}'.format(gsm, gse)
+            flag = False
+    if flag:
+        print 'No duplication detected within any GSE. :)'
+    else:
+        print 'please check the duplicated GSMs. :('
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='detect duplicated GSMs in GSE_GSM.csv')
+    parser.add_argument(
+        '-f', '--input_csv', type=str,
+        help='input GSE_GSM.csv, check check example_GSE_GSM.csv for format ')
+    options = parser.parse_args()
+    return options
+
 
 if __name__ == "__main__":
-    try:
-        batch_csv = sys.argv[1]
-        main(batch_csv)
-    except IndexError, _:
-        print 'Usage: python2.7.x detect_duplicate_GSMs.py batch.csv (e.g. RNAseq_batchn.csv)'
-        sys.exit(1)
+    main()
