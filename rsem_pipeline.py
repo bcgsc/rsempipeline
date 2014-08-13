0383494a5dc2b479b901e126c3d0e17053fb28ef
Zhuyi Xue (bcgsc) <alfred532008@gmail.com>
2014-08-13 11:25:23 -0700

    added execute_mutex function to rsem_pipeline.py instead of using execute directly for ordered output during parallel processing

--- rsem_pipeline.py
+++ rsem_pipeline.py
@@ -46,6 +46,14 @@ UM.init_sample_outdirs(samples, UM.get_rsem_outdir(config, options))
 
 ##################################end of main##################################
 
+def execute_mutex(cmd, msg_id='', flag_file=None, debug=False):
+    """
+    :param msg_id: id for identifying a message
+    """
+    with logger_mutex:
+        U.execute(cmd, msg_id, flag_file, debug)
+
+    
 def originate_params():
     """
     Generate a list of sras to download for each sample
@@ -87,14 +95,14 @@ def download(inputs, outputs, sample):
     # anonftp@ftp-trace.ncbi.nlm.nih.gov:{url_path} {output_dir}
     cmd = config['CMD_ASCP'].format(
         log_dir=sra_outdir, url_path=sra_url_path, output_dir=sra_outdir)
-    returncode = U.execute(cmd, msg_id, flag_file, options.debug)
+    returncode = execute_mutex(cmd, msg_id, flag_file, options.debug)
     if returncode != 0 or returncode is None:
         # try wget
         # cmd template looks like this:
         # wget ftp://ftp-trace.ncbi.nlm.nih.gov{url_path} -P {output_dir} -N
         cmd = config['CMD_WGET'].format(
             url_path=sra_url_path, output_dir=sra_outdir)
-        U.execute(cmd, msg_id, flag_file, options.debug)
+        execute_mutex(cmd, msg_id, flag_file, options.debug)
 
                
 @R.subdivide(
@@ -108,7 +116,7 @@ def sra2fastq(inputs, outputs):
     flag_file = outputs[-1]
     outdir = os.path.dirname(os.path.dirname(os.path.dirname(sra)))
     cmd = config['CMD_FASTQ_DUMP'].format(output_dir=outdir, accession=sra)
-    U.execute(cmd, flag_file=flag_file, debug=options.debug)
+    execute_mutex(cmd, flag_file=flag_file, debug=options.debug)
 
 
 @R.collate(
@@ -203,7 +211,7 @@ def rsem(inputs, outputs):
         reference_name=reference_name,
         sample_name=sample_name,
         output_dir=outdir)
-    U.execute(cmd, flag_file=flag_file, debug=options.debug)
+    execute_mutex(cmd, flag_file=flag_file, debug=options.debug)
 
 if __name__ == "__main__":
     UM.act(options, samples)
