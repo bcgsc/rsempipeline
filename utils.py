8fa842073e8c0bd44d69cd32722db71e0eaf7bf4
Zhuyi Xue (bcgsc) <alfred532008@gmail.com>
2014-08-13 11:20:51 -0700

    updated execute function in utils.py for realtime output

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import select
 import subprocess
 import pickle
 
@@ -70,13 +71,57 @@ def gen_sample_msg_id(sample):
 #         sample.name, sample.index, series.num_passed_samples(),
 #         series.name)
 
+# used a better version of execute as defined in rsem_pipeline.py --2014-08-13
+# def execute(cmd, msg_id='', flag_file=None, debug=False):
+#     logger.info('executing CMD: {0}'.format(cmd))
+#     if debug:                   # only print out cmd
+#         return
+#     try:
+#         returncode = subprocess.call(cmd, shell=True, executable="/bin/bash")
+#         if returncode != 0:
+#             logger.error(
+#                 '{0}, started, but then failed with returncode: {1}. '
+#                 'CMD "{2}"'.format(msg_id, returncode, cmd))
+#         else:
+#             logger.info('{0}, execution succeeded with returncode: {1}. '
+#                         'CMD "{2}"'.format(msg_id, returncode, cmd))
+#             if flag_file is not None:
+#                 touch(flag_file)
+#         return returncode
+#     except OSError, err:
+#         logger.exception(
+#             '{0}, failed to start, raising OSError {1}. '
+#             'CMD: "{2}"'.format(msg_id, err, cmd))
 
 def execute(cmd, msg_id='', flag_file=None, debug=False):
-    logger.info('executing CMD: {0}'.format(cmd))
-    if debug:                   # only print out cmd
+    logger.info(cmd)
+    if debug:
         return
     try:
-        returncode = subprocess.call(cmd, shell=True, executable="/bin/bash")
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE, shell=True,
+                                executable="/bin/bash")
+
+        # stdout, stderr = [], []
+        while True:
+            reads = [proc.stdout.fileno(), proc.stderr.fileno()]
+            ret = select.select(reads, [], [])
+
+            for fd in ret[0]:
+                if fd == proc.stdout.fileno():
+                    read = proc.stdout.readline()
+                    logger.info('stdout: ' + read.strip())
+                    # stdout.append(read)
+                if fd == proc.stderr.fileno():
+                    read = proc.stderr.readline()
+                    logger.info('stderr: ' + read.strip())
+                    # stderr.append(read)
+
+            if proc.poll() != None:
+                break
+
+        returncode = proc.returncode
+
         if returncode != 0:
             logger.error(
                 '{0}, started, but then failed with returncode: {1}. '
@@ -86,7 +131,6 @@ def execute(cmd, msg_id='', flag_file=None, debug=False):
                         'CMD "{2}"'.format(msg_id, returncode, cmd))
             if flag_file is not None:
                 touch(flag_file)
-        return returncode
     except OSError, err:
         logger.exception(
             '{0}, failed to start, raising OSError {1}. '
