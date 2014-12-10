1bdf59d7ae5a8fb0140e093c87e185827e2f3789
Zhuyi Xue <alfred532008@gmail.com>
2014-12-10 15:04:05 -0800

    pruned setup.py

--- setup.py
+++ setup.py
@@ -24,9 +24,6 @@ setup(
     # packages=setuptools.find_packages(exclude=['venv*', 'fq_gz_temp_stats', 'non-vc']),
     packages=find_packages(),
 
-    # List run-time dependencies here.  These will be installed by pip when your
-    # project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
     # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
     install_requires=[
         'beautifulsoup4>=4.3.2',
@@ -36,27 +33,6 @@ setup(
         'ruffus>=2.4.1',
         'requests>=2.3.0',
     ],
-
-    # # List additional groups of dependencies here (e.g. development dependencies).
-    # # You can install these using the following syntax, for example:
-    # # $ pip install -e .[dev,test]
-    # extras_require = {
-    #     'dev': ['check-manifest'],
-    #     'test': ['coverage'],
-    # },
-
-    # # If there are data files included in your packages that need to be
-    # # installed, specify them here.  If using Python 2.6 or less, then these
-    # # have to be included in MANIFEST.in as well.
-    # package_data={
-    #     'sample': ['package_data.dat'],
-    # },
-
-    # # Although 'package_data' is the preferred approach, in some case you may
-    # # need to place data files outside of your packages.
-    # # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
-    # # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
