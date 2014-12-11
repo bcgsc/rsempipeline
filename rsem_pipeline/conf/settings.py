f9181dd4eaced8b89b0759a2768a7144cea6fea1
Zhuyi Xue <alfred532008@gmail.com>
2014-12-11 15:02:55 -0800

    fixed a bug in settings, assigning CONF_DIR

--- rsem_pipeline/conf/settings.py
+++ rsem_pipeline/conf/settings.py
@@ -2,7 +2,7 @@ import os
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 BASE_DIR = os.path.join(PROJECT_DIR, 'rsem_pipeline')
-CONF_DIR = os.path.join(PROJECT_DIR, 'conf')
+CONF_DIR = os.path.join(BASE_DIR, 'conf')
 SHARE_DIR = os.path.join(CONF_DIR, 'share')
 
 RP_RUN_LOGGING_CONFIG = os.path.join(CONF_DIR, 'rp-run.logging.config')
