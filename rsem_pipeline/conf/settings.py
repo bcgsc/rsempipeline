9c6b9e4ea3953938703d3f7d70ec3046dac3fa08
Zhuyi Xue <alfred532008@gmail.com>
2014-12-11 11:40:45 -0800

    added conf/settings.py

---
+++ rsem_pipeline/conf/settings.py
@@ -0,0 +1,9 @@
+import os
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.join(PROJECT_DIR, 'rsem_pipeline')
+CONF_DIR = os.path.join(PROJECT_DIR, 'conf')
+
+RP_RUN_LOGGING_CONFIG = os.path.join(CONF_DIR, 'rp-run.logging.config')
+RP_TRANSFER_LOGGING_CONFIG = os.path.join(CONF_DIR, 'rp-transfer.logging.config')
+
