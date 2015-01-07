088521f95293f714056679d005ef6244fae37fb3
Zhuyi Xue <alfred532008@gmail.com>
2015-01-07 13:29:32 -0800

    put RP_PREP_LOGGING_CONFIG in rsempipeline/conf/settings.py

--- rsempipeline/conf/settings.py
+++ rsempipeline/conf/settings.py
@@ -5,6 +5,7 @@ BASE_DIR = os.path.join(PROJECT_DIR, 'rsempipeline')
 CONF_DIR = os.path.join(BASE_DIR, 'conf')
 SHARE_DIR = os.path.join(CONF_DIR, 'share')
 
+RP_PREP_LOGGING_CONFIG = os.path.join(CONF_DIR, 'rp-prep.logging.config')
 RP_RUN_LOGGING_CONFIG = os.path.join(CONF_DIR, 'rp-run.logging.config')
 RP_TRANSFER_LOGGING_CONFIG = os.path.join(CONF_DIR, 'rp-transfer.logging.config')
 
