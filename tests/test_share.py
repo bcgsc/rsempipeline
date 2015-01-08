81b13264571bce58657922977c2f3932867e9ae4
Zhuyi Xue <alfred532008@gmail.com>
2015-01-08 14:26:09 -0800

    added test_share.py

---
+++ tests/test_share.py
@@ -0,0 +1,17 @@
+import os
+import unittest
+
+from rsempipeline.preprocess import utils
+from rsempipeline.conf.settings import SHARE_DIR
+
+
+class ShareTestCase(unittest.TestCase):
+    def test_GSE_GSM_example_csv_exits(self):
+        self.assertTrue(os.path.exists(os.path.join(SHARE_DIR,  'GSE_GSM.example.csv')))
+
+    def test_rp_config_example_yml_exists(self):
+        self.assertTrue(os.path.exists(os.path.join(SHARE_DIR, 'rp_config.example.yml')))
+
+
+if __name__ == "__main__":
+    unittest.main()
