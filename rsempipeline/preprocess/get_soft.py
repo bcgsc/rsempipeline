9438d42ce21f691fd4f81a16fde7cb4f75cddd38
Zhuyi Xue <alfred532008@gmail.com>
2015-01-15 17:16:31 -0800

    added further tests to test_preprocess_get_soft.py

--- rsempipeline/preprocess/get_soft.py
+++ rsempipeline/preprocess/get_soft.py
@@ -27,29 +27,26 @@ class SOFTDownloader(object):
         self.ftp_handler = FTP(domain)
         self.ftp_handler.login()
         self.base = 'ftp://{0}'.format(domain)
-
-    def get_soft_file(self, gse, outdir):
-        return os.path.join(outdir, '{0}_family.soft.subset'.format(gse))
         
     def gen_soft(self, gse, outdir):
         """
         @param gse: GSE ID, e.g. GSE45284
         @param soft_dir: directory where soft is to be saved
         """
-        soft_subset = self.get_soft_file(gse, outdir)
+        soft_subset = self.get_soft_subset(gse, outdir)
         if os.path.exists(soft_subset):
             logger.info('{0} has already existed'.format(soft_subset))
         else:
-            soft_gzip = self.download_soft_gz(gse, outdir)
-            if soft_gzip:       # meaning downloading successful
-                soft_subset = self.gunzip_and_extract_soft(soft_gzip)
-                logger.info('removing {0}'.format(soft_gzip))
-                os.remove(soft_gzip)
+            soft_gz = self.download_soft_gz(gse, outdir)
+            if soft_gz:       # meaning downloading successful
+                self.gunzip_and_extract_soft(soft_gz, soft_subset)
+                logger.info('removing {0}'.format(soft_gz))
+                os.remove(soft_gz)
                 return soft_subset
 
     def download_soft_gz(self, gse, outdir):
         """download soft.gzip file"""
-        path = self.get_path(gse)
+        remote_path = self.get_remote_path(gse)
         soft_gz = self.get_soft_gz_basename(gse)
         local_soft_gz = os.path.join(outdir, soft_gz)
         url = self.get_url(gse) # for logging purpose only
@@ -57,7 +54,8 @@ class SOFTDownloader(object):
         logger.info('downloading {0} from {1} to '
                     '{2}'.format(soft_gz, url, local_soft_gz))
         try:
-            self.retrieve(path, soft_gz, local_soft_gz)
+            self.retrieve(remote_path, soft_gz, local_soft_gz)
+            return local_soft_gz
         except Exception:
             logger.exception('error when downloading {0}'.format(url))            
 
@@ -67,28 +65,29 @@ class SOFTDownloader(object):
             cmd = 'RETR {0}'.format(filename)
             self.ftp_handler.retrbinary(cmd, opf.write)
 
+    def get_soft_subset(self, gse, outdir):
+        """soft.subset with path"""
+        return os.path.join(outdir, '{0}_family.soft.subset'.format(gse))
+
     def get_soft_gz_basename(self, gse):
         return '{0}_family.soft.gz'.format(gse)
 
     def get_gse_mask(self, gse):
         return gse[0:-3] + 'nnn'
 
-    def get_path(self, gse):
+    def get_remote_path(self, gse):
         mask = self.get_gse_mask(gse)
         return os.path.join('/', 'geo', 'series', mask, gse, 'soft')
     
     def get_url(self, gse):
-        path = self.get_path(gse)
+        path = self.get_remote_path(gse)
         soft_gz = self.get_soft_gz_basename(gse)
         return urlparse.urljoin(self.base, os.path.join(path, soft_gz))
 
-    def gunzip_and_extract_soft(self, soft_gz):
+    def gunzip_and_extract_soft(self, soft_gz, soft_subset):
         """gunzip soft.gzip and extract its content to generate soft.subset"""
-        dirname, basename = os.path.split(soft_gz)
-        soft_subset = os.path.join(
-            dirname, basename.replace('.gz', '') + '.subset')
-        logger.info('gunziping and extracting from {0} to {1}'.format(
-            soft_gz, soft_subset))
+        logger.info('gunziping and extracting from '
+                    '{0} to {1}'.format(soft_gz, soft_subset))
         with open(soft_subset, 'wb') as opf:
             with gzip.open(soft_gz, 'rb') as inf:
                 for line in inf:
@@ -102,8 +101,6 @@ class SOFTDownloader(object):
                         line.startswith('!Sample_instrument_model') or
                         line.startswith('!Sample_library_source')):
                         opf.write(line)
-        return soft_subset
-
 
 def gen_soft_outdir(outdir):
     d = os.path.join(outdir, SOFT_OUTDIR_BASENAME)
--- tests/test_preprocess_get_soft.py
+++ tests/test_preprocess_get_soft.py
@@ -47,8 +47,8 @@ class SOFTDownloaderTestCase(unittest.TestCase):
         self.assertEqual(self.der.get_gse_mask(self.gse1), 'GSE45nnn')
         self.assertEqual(self.der.get_gse_mask(self.gse2), 'GSE12345nnn')
 
-    def test_get_path(self):
-        self.assertEqual(self.der.get_path(self.gse1),
+    def test_get_remote_path(self):
+        self.assertEqual(self.der.get_remote_path(self.gse1),
                          '/geo/series/GSE45nnn/GSE45284/soft')
 
     def test_get_url(self):
@@ -82,21 +82,99 @@ class SOFTDownloaderTestCase(unittest.TestCase):
     @mock.patch.object(get_soft.SOFTDownloader, 'retrieve')
     @log_capture()
     def test_download_soft_gz(self, mock_retrieve, L):
-        self.der.download_soft_gz(self.gse1, 'any_outdir')
-        args = '/geo/series/GSE45nnn/GSE45284/soft', 'GSE45284_family.soft.gz', 'any_outdir/GSE45284_family.soft.gz'
+        res = self.der.download_soft_gz(self.gse1, 'any_outdir')
+        self.assertEqual(res, 'any_outdir/GSE45284_family.soft.gz')
+        args = ('/geo/series/GSE45nnn/GSE45284/soft', 'GSE45284_family.soft.gz', res)
         mock_retrieve.assert_called_once_with(*args)
         L.check(('rsempipeline.preprocess.get_soft', 'INFO',
                  'downloading GSE45284_family.soft.gz from ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE45nnn/GSE45284/soft/GSE45284_family.soft.gz to any_outdir/GSE45284_family.soft.gz'))
 
+        # when retrieve raises Exception
         mock_retrieve.side_effect = Exception()
-        self.der.download_soft_gz(self.gse1, 'any_outdir')
+        res = self.der.download_soft_gz(self.gse1, 'any_outdir')
+        self.assertIsNone(res)
         mock_retrieve.assert_called_with(*args)
         self.assertEqual(mock_retrieve.call_count, 2)
         self.assertIn('error when downloading', str(L))
 
-    def test_get_soft_file(self):
-        self.assertEqual(self.der.get_soft_file(self.gse1, 'any_outdir'),
+    def test_get_soft_subset(self):
+        self.assertEqual(self.der.get_soft_subset(self.gse1, 'any_outdir'),
                          'any_outdir/GSE45284_family.soft.subset')
+
+    @mock.patch.object(get_soft.gzip, 'open')
+    @mock.patch('rsempipeline.preprocess.get_soft.os.path')
+    @log_capture()
+    def test_gunzip_and_extract_soft(self, mock_path, mock_gzip_open, L):
+        soft_gz = 'any_dir/the.soft.gz'
+        soft_subset = 'any_dir/the.soft.subset'
+        mock_open = mock.mock_open()
+        with mock.patch('rsempipeline.preprocess.get_soft.open', mock_open, create=True):
+            self.der.gunzip_and_extract_soft(soft_gz, soft_subset)
+        mock_open.assert_called_once_with(soft_subset, 'wb')
+        mock_gzip_open.assert_called_once_with(soft_gz, 'rb')
+        L.check(('rsempipeline.preprocess.get_soft', 'INFO',
+                 'gunziping and extracting from any_dir/the.soft.gz to any_dir/the.soft.subset'))
+
+    # def test_gunzip_and_extract_soft_with_real_gz_files(self):
+    #     # to be written
+    #     pass
+
+    @mock.patch.object(get_soft.os.path, 'exists')
+    @mock.patch.object(get_soft.SOFTDownloader, 'download_soft_gz', autospec=True)
+    @log_capture()
+    def test_gen_soft_with_already_existing_soft_subset(
+            self, mock_download_soft_gz, mock_exists, L):
+        mock_exists.return_value = True
+        self.der.gen_soft(self.gse1, 'any_outdir')
+        self.assertFalse(mock_download_soft_gz.called)
+        L.check(('rsempipeline.preprocess.get_soft', 'INFO',
+                 'any_outdir/GSE45284_family.soft.subset has already existed'))
+
+
+    @mock.patch.object(get_soft.os, 'remove')
+    @mock.patch.object(get_soft.os.path, 'exists')
+    @mock.patch.object(get_soft.SOFTDownloader, 'gunzip_and_extract_soft')
+    @mock.patch.object(get_soft.SOFTDownloader, 'download_soft_gz')
+    @log_capture()
+    def test_gen_soft_without_existing_soft_subset_with_successful_download_soft_gz(
+            self,
+            mock_download_soft_gz,
+            mock_gunzip_and_extract_soft,
+            mock_exists,
+            mock_remove,
+            L):
+        gz = 'any_outdir/GSE45284_family.soft.gz'
+        subset = 'any_outdir/GSE45284_family.soft.subset'
+        mock_exists.return_value = False
+        mock_download_soft_gz.return_value = gz
+        res = self.der.gen_soft(self.gse1, 'any_outdir')
+        self.assertEqual(res, subset)
+        mock_download_soft_gz.assert_called_with(self.gse1, 'any_outdir')
+        mock_gunzip_and_extract_soft.assert_called_with(gz, subset)
+        mock_remove.assert_called_with(gz)
+        L.check(('rsempipeline.preprocess.get_soft', 'INFO',
+                 'removing any_outdir/GSE45284_family.soft.gz'))
+
+
+    @mock.patch.object(get_soft.os, 'remove')
+    @mock.patch.object(get_soft.os.path, 'exists')
+    @mock.patch.object(get_soft.SOFTDownloader, 'gunzip_and_extract_soft')
+    @mock.patch.object(get_soft.SOFTDownloader, 'download_soft_gz')
+    @log_capture()
+    def test_gen_soft_without_existing_soft_subset_with_unsuccessful_download_soft_gz(
+            self,
+            mock_download_soft_gz,
+            mock_gunzip_and_extract_soft,
+            mock_exists,
+            mock_remove,
+            L):
+        mock_exists.return_value = False
+        mock_download_soft_gz.return_value = None
+        res = self.der.gen_soft(self.gse1, 'any_outdir')
+        self.assertIsNone(res)
+        mock_download_soft_gz.assert_called_with(self.gse1, 'any_outdir')
+        self.assertFalse(mock_gunzip_and_extract_soft.called)
+        self.assertFalse(mock_remove.called)
 
 
 if __name__ == "__main__":
