71efc0270388c2bbfb97e0c3294b8e2b1d1e278b
Zhuyi Xue <alfred532008@gmail.com>
2015-08-04 10:11:13 -0700

    added tests/test_core/test_rp_run.py

---
+++ tests/test_core/test_rp_run.py
@@ -0,0 +1,10 @@
+import unittest
+import mock
+
+from testfixtures import log_capture
+
+from rsempipeline.utils.objs import Series, Sample
+
+class RPRunTestCase(unittest.TestCase):
+    def test(self):
+        pass
